329a8db0ec59db269379e52896d2f1bd9e207085
isnendyankp <isnendyankp@gmail.com>
2023-08-14 12:03:13 +0700

    S7-182: cr8 destructing assignment object people

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import people from './data';
 
 const App = () => {
   const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
   return <h2>Reviews Starter</h2>;
 };
 export default App;
 
 // - import people
 // - cr8 useState index
+// - cr8 destructing assignment from object people. make object people have many properties (name, job,img, text)

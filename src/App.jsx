da18581395efab81cd118682ab4a4c291b6bb9bc
isnendyankp <isnendyankp@gmail.com>
2023-08-14 12:26:53 +0700

    S7-183:import state, Faber & navigate Faber to return

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { useState } from 'react';
 import people from './data';
+import { Faber } from 'react-icons/fa';
 
 const App = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
-  return <h2>Reviews Starter</h2>;
+  return(
+    <div>
+      <h2>Review Starter</h2>
+      <Faber />
+    </div>
+  );
 };
 export default App;
 
 // - import people
 // - cr8 useState index
 // - cr8 destructing assignment from object people. make object people have many properties (name, job,img, text)
+// - import state, Faber & navigate Faber to return

4f9b126f1d217e74c89d6657e8b50b182c283b73
isnendyankp <isnendyankp@gmail.com>
2023-08-14 11:59:42 +0700

    S7-182: import people & cr8 useState index

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
+import people from './data';
+
 const App = () => {
+  const [index, setIndex] = useState(0);
   return <h2>Reviews Starter</h2>;
 };
 export default App;
+
+// - import people
+// - cr8 useState index
